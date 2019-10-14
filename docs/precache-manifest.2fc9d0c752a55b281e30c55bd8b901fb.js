5967803458b3f13aa5782d2482feba919a4e4c04
dmoriley <david.m.oriley@hotmail.com>
2019-10-14 17:16:45 -0400

    docs folder for github pages

---
+++ docs/precache-manifest.2fc9d0c752a55b281e30c55bd8b901fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec241062b64b04b05cd8d738d2a98160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5456c7371c567fe1710f",
+    "url": "/static/css/main.7d05f28d.chunk.css"
+  },
+  {
+    "revision": "cf0189519603f828c574",
+    "url": "/static/js/2.a777636d.chunk.js"
+  },
+  {
+    "revision": "5456c7371c567fe1710f",
+    "url": "/static/js/main.025c3893.chunk.js"
+  },
+  {
+    "revision": "0557ad5ad50525cf2a8a",
+    "url": "/static/js/runtime-main.c94a26bb.js"
+  }
+]);
